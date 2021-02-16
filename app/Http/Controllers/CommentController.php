b9efb0e81ecc0904ea016bd4304c8de5c5c68ccc
Jaycee Mariano <alohajaycee@gmail.com>
2021-02-16 21:55:53 +0800

    Create cmment crud

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,19 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CommentCreateRequest;
+use App\Http\Requests\CommentEditRequest;
 
 class CommentController extends Controller
 {
+    public const COMMENTABLE = [
+        'task'      => 'App\Models\Task',
+        'project'   => 'App\Models\Project',
+        'log'       => 'App\Models\Log',
+    ];
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request, $commentable, $uuid)
     {
-        //
+        $$commentable = self::COMMENTABLE[$commentable]::where('uuid', $uuid)->firstOrFail();
+
+        $comments = $$commentable->comments()->latest()->simplePaginate();
+
+        return response($comments);
     }
 
     /**
@@ -22,20 +36,13 @@ class CommentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(CommentCreateRequest $request, $commentable, $uuid)
     {
-        //
-    }
+        $$commentable = self::COMMENTABLE[$commentable]::where('uuid', $uuid)->firstOrFail();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $comment = $$commentable->comments()->save($request->validated());
+
+        return response($comment, 201);
     }
 
     /**
@@ -46,7 +53,7 @@ class CommentController extends Controller
      */
     public function show(Comment $comment)
     {
-        //
+        return response($comment);
     }
 
     /**
@@ -55,21 +62,11 @@ class CommentController extends Controller
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function edit(Comment $comment)
+    public function edit(CommentEditRequest $request ,Comment $comment)
     {
-        //
-    }
+        $comment->update($request->validated());
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Comment $comment)
-    {
-        //
+        return response($comment);
     }
 
     /**
@@ -80,6 +77,10 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
-        //
+        $comment->delete();
+
+        return response([
+            'message' => __('task.delete_success')
+        ]);
     }
 }
