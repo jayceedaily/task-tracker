2e0582418dcfcf9b46c355f7658ab7d6f922125f
Jaycee Mariano <alohajaycee@gmail.com>
2021-03-14 20:35:10 +0800

    remove comment

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -17,11 +17,6 @@ class ProjectController extends Controller
                 ->projects()
                 ->latest()
                 ->simplePaginate();
-
-
-        // return vsprintf(str_replace('?', '%s', $query->toSql()), collect($query->getBindings())->map(function ($binding) {
-        //     return is_numeric($binding) ? $binding : "'{$binding}'";
-        // })->toArray());
     }
 
     public function create(CreateRequest $request)
